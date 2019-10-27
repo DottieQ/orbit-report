1dab7d8b78a7702001f1bfa2cd4bcac6d7572657
Dottie <dottiequick@gmail.com>
2019-10-26 22:57:05 -0500

    started part 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,11 @@ export class AppComponent {
   title = 'orbit-report';
 
   sourceList: Satellite[];
+  displayList: Satellite[];
   
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -31,14 +33,24 @@ export class AppComponent {
             };
             this.sourceList.push(satellite);
           }
-
-
-          // TODO: loop over satellites
-          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
- 
+          this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
+    
  
  }
+
+ 
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  this.displayList = matchingSatellites;
+}
+
 }
